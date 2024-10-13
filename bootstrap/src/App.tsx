cc54aa0d6a1458ca006d4fb46072d5c92d761742
muneeb-mp <phansopkarmuneeb@gmail.com>
2024-10-13 17:13:25 +0530

    Added grid layout in react vite app content wrapper

--- bootstrap/src/App.tsx
+++ bootstrap/src/App.tsx
@@ -4,7 +4,38 @@ import Layout from "./layout/Layout";
 function App() {
   return (
     <Layout>
-      <div>Hello World</div>
+      <div className="cards">
+        <div className="card grid-span-col-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          dolorem iure quas nostrum eum deserunt quia in? Nemo pariatur,
+          perspiciatis cupiditate laudantium error minima tenetur rem dolor
+          doloremque est dolores?
+        </div>
+        <div className="card">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          dolorem iure quas nostrum eum deserunt quia in? Nemo pariatur,
+          perspiciatis cupiditate laudantium error minima tenetur rem dolor
+          doloremque est dolores?
+        </div>
+        <div className="card">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          dolorem iure quas nostrum eum deserunt quia in? Nemo pariatur,
+          perspiciatis cupiditate laudantium error minima tenetur rem dolor
+          doloremque est dolores?
+        </div>
+        <div className="card grid-span-col-2">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          dolorem iure quas nostrum eum deserunt quia in? Nemo pariatur,
+          perspiciatis cupiditate laudantium error minima tenetur rem dolor
+          doloremque est dolores?
+        </div>
+        <div className="card">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+          dolorem iure quas nostrum eum deserunt quia in? Nemo pariatur,
+          perspiciatis cupiditate laudantium error minima tenetur rem dolor
+          doloremque est dolores?
+        </div>
+      </div>
     </Layout>
   );
 }
